21c77b6e00593e39431c62deedb2b808018f3086
cristianiniguez <felizpc1@gmail.com>
2022-05-21 14:32:12 -0400

    sistema de formularios: formulario de login

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,13 +1,36 @@
-import { View, Text } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
-type Props = {};
-
-const LoginForm = (props: Props) => {
+const LoginForm = () => {
   return (
     <View>
-      <Text>LoginForm</Text>
+      <Text style={styles.title}>Iniciar sesión</Text>
+      <TextInput autoCapitalize='none' placeholder='Nombre de usuario' style={styles.input} />
+      <TextInput
+        autoCapitalize='none'
+        placeholder='Contraseña'
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button title='Entrar' onPress={() => console.log('Entrando')} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
 
 export default LoginForm;

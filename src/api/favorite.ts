864fdc9a8840c3e80af9f4163b1385cbb591adae
Cristian Iñiguez <felizpc1@gmail.com>
2022-06-06 21:19:27 -0400

    sistema de favoritos: eliminar pokemon de favoritos

--- src/api/favorite.ts
+++ src/api/favorite.ts
@@ -19,3 +19,9 @@ export async function isPokemonFavorite(id: string) {
   const favorites = await getPokemonFavorites();
   return includes(favorites, id);
 }
+
+export async function removePokemonFromFavorites(id: string) {
+  const favorites = await getPokemonFavorites();
+  const newFavorites = pull(favorites, id);
+  await AsyncStorage.setItem(FAVORITE_STORAGE, JSON.stringify(newFavorites));
+}
--- src/components/pokemon/PokemonFavorite.tsx
+++ src/components/pokemon/PokemonFavorite.tsx
@@ -1,6 +1,10 @@
 import { FC, useEffect, useState } from 'react';
 import Icon from '@expo/vector-icons/FontAwesome5';
-import { addPokemonToFavorites, isPokemonFavorite } from '../../api/favorite';
+import {
+  addPokemonToFavorites,
+  isPokemonFavorite,
+  removePokemonFromFavorites,
+} from '../../api/favorite';
 
 type PokemonFavoriteProps = {
   id: string;
@@ -20,12 +24,21 @@ const PokemonFavorite: FC<PokemonFavoriteProps> = ({ id }) => {
   };
 
   const addToFavorites = async () => {
-    await addPokemonToFavorites(id);
-    onReloadCheckFavorite();
+    try {
+      await addPokemonToFavorites(id);
+      onReloadCheckFavorite();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const removeFromFavorites = async () => {
-    console.log('Elimina de favoritos');
+    try {
+      await removePokemonFromFavorites(id);
+      onReloadCheckFavorite();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
